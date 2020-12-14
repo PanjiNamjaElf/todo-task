13967a09c51cf308a00825d89350dbb423836f9d
Panji Setya Nur Prawira <kstar.panjinamjaelf@gmail.com>
2020-12-14 15:50:14 +0700

    feat: Create task model factory

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 /**
@@ -26,5 +27,5 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Task extends Model
 {
-
+    use HasFactory;
 }
