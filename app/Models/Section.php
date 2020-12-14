467a51100c80c14c4e6473c11d91d1ed8ac44893
Panji Setya Nur Prawira <kstar.panjinamjaelf@gmail.com>
2020-12-14 17:58:40 +0700

    feat: Add soft deletes trait

--- app/Models/Section.php
+++ app/Models/Section.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * App\Models\Section
@@ -13,9 +14,12 @@ use Illuminate\Database\Eloquent\Model;
  * @property string $description
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
- * @property string|null $deleted_at
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Task[] $tasks
+ * @property-read int|null $tasks_count
  * @method static \Illuminate\Database\Eloquent\Builder|Section newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Section newQuery()
+ * @method static \Illuminate\Database\Query\Builder|Section onlyTrashed()
  * @method static \Illuminate\Database\Eloquent\Builder|Section query()
  * @method static \Illuminate\Database\Eloquent\Builder|Section whereCreatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Section whereDeletedAt($value)
@@ -23,11 +27,14 @@ use Illuminate\Database\Eloquent\Model;
  * @method static \Illuminate\Database\Eloquent\Builder|Section whereId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Section whereName($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Section whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|Section withTrashed()
+ * @method static \Illuminate\Database\Query\Builder|Section withoutTrashed()
  * @mixin \Eloquent
  */
 class Section extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     public function tasks() {
         return $this->hasMany(Task::class)->latest();
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,31 +4,40 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Str;
 
 /**
  * App\Models\Task
  *
  * @property int $id
+ * @property int $section_id
  * @property string $name
  * @property string $description
  * @property int $is_done
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read mixed $excerpt
+ * @property-read \App\Models\Section $section
  * @method static \Illuminate\Database\Eloquent\Builder|Task newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Task newQuery()
+ * @method static \Illuminate\Database\Query\Builder|Task onlyTrashed()
  * @method static \Illuminate\Database\Eloquent\Builder|Task query()
  * @method static \Illuminate\Database\Eloquent\Builder|Task whereCreatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Task whereDescription($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Task whereId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Task whereIsDone($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Task whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Task whereSectionId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Task whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|Task withTrashed()
+ * @method static \Illuminate\Database\Query\Builder|Task withoutTrashed()
  * @mixin \Eloquent
  */
 class Task extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     public function section()
     {
